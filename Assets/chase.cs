41a4924c9dd5b5654fa9674cb0ceef38e260134b
Avery <44121063+Avery007@users.noreply.github.com>
2021-02-23 18:07:47 -0500

    added stuff

---
+++ Assets/chase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    void Start()
+    {
+        
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F) == false)
+        {
+            //this.transform.position += (this.transform.position - target.position).normalized * speed * Time.deltaTime;
+            //this.transform.position = Vector3.Lerp(this.transform.position, target.position, 0.1f);
+            this.transform.Translate(Vector3.Normalize(target.position - this.transform.position) * 0.005f);
+        }
+    }
+}
